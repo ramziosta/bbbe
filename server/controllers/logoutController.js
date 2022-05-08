4593c6e0c05ef04080d7de59016b8cdc4d066ef6
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-05-08 02:06:07 -0400

    logout not working good

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -1,26 +1,41 @@
-const User = require('../models/User');
-
-const handleLogout = async (req, res) => {
+const clientDB = {
+    clients: require("../data/clients.json"),
+    setClients: function (data) {
+      this.clients = data;
+    },
+  };
+  
+  const fs = require("fs").promises;
+  const path = require("path");
+  
+  const handleLogout = async (req, res) => {
     // On client, also delete the accessToken
-
+  
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(204); //No content
     const refreshToken = cookies.jwt;
-
+  
     // Is refreshToken in db?
-    const foundUser = await User.findOne({ refreshToken }).exec();
-    if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    const foundClient = clientDB.clients.find(person => person.refreshToken === refreshToken);
+    if (!foundClient) {
+        res.clearCookie('jwt', {httpOnly: true});
         return res.sendStatus(204);
     }
 
-    // Delete refreshToken in db
-    foundUser.refreshToken = '';
-    const result = await foundUser.save();
-    console.log(result);
+  // Delete refreshToken in db
 
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+  const otherClients = clientDB.clients.filter(
+    (person) => person.refreshToken !== foundClient.refreshToken
+  );
+  const currentClient = { ...foundClient, refreshToken: '' };
+  clientDB.setClients([...otherClients, currentClient]);
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "data", "clients.json"),
+    JSON.stringify(clientDB.clients)
+  );
+    res.clearCookie('jwt', {httpOnly: true}); //add secure: true in production to use https instead of http
     res.sendStatus(204);
-}
 
-module.exports = { handleLogout }
+  }
+
+module.exports = { handleLogout };
